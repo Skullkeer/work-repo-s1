52f581876de493eddf3f757b49ea86edbc42dcee
Willoww <0654313@schoolsnet.act.edu.au>
2026-06-16 15:56:54 +1000

    item 4 practice added

---
+++ item4-practice/q4e_response.sql
@@ -0,0 +1,30 @@
+/*
+    SQL submission file
+
+    ITEM 4 (PRACTICE - TERTIARY) - QUESTION 4
+
+    Provide your responses for Q4 below.
+    Unless otherwise indicated, no other files will be assessed for this question.
+
+    If you delete this file or it is otherwise missing from your submission,
+    you will automatically score 0 for the question.
+    If you think you are missing a file, ask your teacher for help.
+
+    Test your queries! If a database is provided, run them there and fix any errors.
+    If a database is not provided, create a simple one if you have the time.
+    ** Queries which do not execute or return errors will be heavily penalized! **
+
+    Avoid the use of any SQL features or functions not covered in our lesson notes.
+    When in doubt about whether something is allowed, ask your teacher.
+
+    Comment your SQL code if it helps you, but only working code will earn marks.
+    Do your best to write correct and readable SQL, even if it is not perfect.
+
+    When writing your SQL queries or schema, prioritize:
+    1. Correctness - the queries/definitions should match the requirements of the question
+    2. Simplicity - avoid complex syntax when simpler alternatives are available
+    3. Style - use clear formatting, indentation, and consistent naming and casing
+*/
+
+-- a)
+
